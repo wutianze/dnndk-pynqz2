fbaac9cb50697c251b2cb9b7977be1bc21c1b72e
sauron <1369130123@qq.com>
2019-11-29 17:44:14 +0800

    take.py updated

--- yolo_keras/prepare_training_data/take.py
+++ yolo_keras/prepare_training_data/take.py
@@ -4,10 +4,10 @@ import argparse
 
 parser = argparse.ArgumentParser("description='--num:how many imgs to take, --category:class name now take'")
 parser.add_argument("--num", type=int,default=200)
-parser.add_argument("--dir", type=str,default='images')
+parser.add_argument("--dir", type=str,default='images2')
 args = parser.parse_args()
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 cap.set(3,416)
 cap.set(4,416)
 i = 0
@@ -19,6 +19,9 @@ while(i < args.num):
     if k == 27:
         break
     if k == ord('s'):
+        flag = not flag
+    if flag:
+        time.sleep(1)
         ticks = time.time()
         cv2.imwrite("./"+args.dir+"/"+str(ticks)+".jpg",frame)
         i = i + 1
